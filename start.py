bb01a421a72c1f39c4a1a77f30032730454e91c4
Nayyir Ahmed <nayyir.ahmed1028@gmail.com>
2024-09-14 18:06:48 -0500

    mouse movement works!!!!

--- start.py
+++ start.py
@@ -75,13 +75,38 @@ def run():
     shipTxtbox = TextBox(background, middle.x - 30, middle.y + 200, 60, 80, fontSize=50, onSubmit=txtCb)
     shipTxtbox.onSubmitParams = [shipTxtbox.text]
     boardGrid = Board()
+
+
+    ships = []
+    #these lines draw boxes for testing mouse movement --> either replace w/ ship object or delete
+    for i in range(5):
+        ship = pg.Rect(50 + i*40, 40, 30, 20)
+        ships.append(ship)
+    active_ship = None
+
     while running:
         # poll for events
-        # pygame.QUIT event means the user clicked X to close the window
+        # pg.QUIT event means the user clicked X to close the window
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
                 running = False
+
+            #mouse movement
+            if event.type == pg.MOUSEBUTTONDOWN: #if mouse clicked
+                for num, ship in enumerate(ships): #track index # of ships
+                    if ship.collidepoint(event.pos): #checks for collision w/ mouse coords
+                        active_ship = num #if collide, update active_ship w/ ship's index val
+                        break
+
+            if event.type == pg.MOUSEMOTION and active_ship != None: #if mouse moved and there is active ship
+                ships[active_ship].move_ip(event.rel) #pick ship from list and move it by same amount as mouse
+
+            if event.type == pg.MOUSEBUTTONUP: #if mouse released
+                if active_ship is not None: #if ship active
+                    active_ship = None #releases ship
+                    break
+            
 
         # fill the screen with a color to wipe away anything from last frame
         background.fill("grey")
@@ -138,43 +163,19 @@ def run():
             text_surface = my_font.render('PLAYER 1, PLACE SHIPS', False, (0, 0, 0))
             background.blit(text_surface, (middle.x - text_surface.get_width()/2, 30))
 
-            #testing mouse movement
-            #these lines just draw some squares for testing
-            boxes = []
-            for i in range(5):
-                box = pg.Rect(50 + i*40, 40, 30, 20)
-                boxes.append(box)
-            active_box = None
-            for box in boxes:
-                pg.draw.rect(background, "purple", box)
+            #draw boxes ----> will have to replace w/ ship object       
+            for ship in ships:
+                pg.draw.rect(background, "purple", ship)
+            
 
-            for event in pg.event.get():
-                if event.type == pg.MOUSEBUTTONDOWN: #if mouse clicked
-                    #if event.button == 1: #if left mouse button clicked
-                    for num, box in enumerate(boxes): #track index # of boxes
-                        if box.collidepoint(event.pos): #checks for collision w/ mouse coords
-                            active_box = num #if collide, update active_box w/ box's index val
-                if event.type == pg.MOUSEMOTION: #if mouse moved
-                    if active_box != None: #check if box has been clicked on and checks its number
-                        boxes[active_box].move_ip(event.rel) #pick box from list and move it by same amount as mouse
-                if event.type == pg.MOUSEBUTTONUP: #if mouse released
-                    if event.button == 1:
-                        active_box = None
-            for box in boxes:
-                pg.draw.rect(background, "purple", box)
-            pg.display.flip()
-        else:
-            pass
-
-        pw.update(events)  # Call once every loop to allow widgets to render and listen
         screen.blit(background, (0, 0))
-
+        pw.update(events)  # Call once every loop to allow widgets to render and listen
+        
         # flip() the display to put the work we did on screen
-        pg.display.update()
-        # pg.display.flip()
+        #pg.display.update()
+        pg.display.flip()
 
-        # limits FPS to 60
-        tick = clock.tick(60)
+        tick = clock.tick(60) # limits FPS to 60
 
     pg.quit()
 
