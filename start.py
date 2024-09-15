23b4e26166bda0117e926d6a833879b765a51347
DmendezJ <damian.mendez@ku.edu>
2024-09-15 15:33:28 -0500

    Stripped down version with all the correct logic

--- start.py
+++ start.py
@@ -1,233 +1,276 @@
-# import platform
-import pygame as pg
-import pygame_widgets as pw
-from pygame_widgets.textbox import TextBox
-# import class objects
-from battleship.button import ClickableButton
-from battleship.board import Board
-from enum import Enum
-import os
-import re
-from battleship.board import Ship
+import pygame
+import random
+import time
 
-class State(Enum):
-    START = 1
-    PICK_SHIPS = 2
-    PLAYER1START = 3
-    PLAYER2START = 4
-    PLAYER1TURN = 5
-    PLAYER2TURN = 6
+# Initialize Pygame
+pygame.init()
 
-game_state = State.START
-num_ships = 1
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+GRID_SIZE = 10
+CELL_SIZE = 40
+MARGIN = 50
 
-main_dir = os.path.split(os.path.abspath(__file__))[0]
-data_dir = os.path.join(main_dir, "data")
-# functions to create our resources
-def load_image(name, scale=1):
-    fullname = os.path.join(data_dir, name)
-    image = pg.image.load(fullname)
-    image = image.convert()
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GRAY = (128, 128, 128)
+GREEN = (0, 255, 0)
 
-    # size = image.get_size()
-    # size = (size[0] * scale, size[1] * scale)
-    # image = pg.transform.scale(image, size)
+# Create the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Battleship")
 
-    return image, image.get_rect()
+class Ship:
+    def __init__(self, size):
+        self.size = size
+        self.hits = 0
+        self.positions = []
 
-def run():
-    # pygame setup
-    pg.init()
-    # set screen size
-    screen = pg.display.set_mode((1280, 720))
-    screen.fill("grey")
-    background = pg.Surface(screen.get_size())
-    background = background.convert()
-    background.fill("grey")
+    def is_sunk(self):
+        return self.hits == self.size
 
-    screen.blit(background, (0,0))
-    pg.display.update()
+class Board:
+    def __init__(self):
+        self.grid = [[0 for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+        self.ships = []
 
-    global game_state
-    game_state = State.START
-    global num_ships
-    num_ships = 1
+    def place_ship(self, ship, x, y, horizontal):
+        if horizontal:
+            if x + ship.size > GRID_SIZE:
+                return False
+            for i in range(ship.size):
+                if self.grid[y][x + i] != 0:
+                    return False
+            for i in range(ship.size):
+                self.grid[y][x + i] = ship
+                ship.positions.append((x + i, y))
+        else:
+            if y + ship.size > GRID_SIZE:
+                return False
+            for i in range(ship.size):
+                if self.grid[y + i][x] != 0:
+                    return False
+            for i in range(ship.size):
+                self.grid[y + i][x] = ship
+                ship.positions.append((x, y + i))
+        self.ships.append(ship)
+        return True
 
-    pg.display.set_caption("Battleship")
-    clock = pg.time.Clock() # keep to limit framerate
-    running = True # track if loop should keep running
+    def receive_attack(self, x, y):
+        if self.grid[y][x] == 0:
+            self.grid[y][x] = -1  # Miss
+            return False
+        elif isinstance(self.grid[y][x], Ship):
+            ship = self.grid[y][x]
+            ship.hits += 1
+            self.grid[y][x] = -2  # Hit
+            return True
+        return False
 
-    middle = pg.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+def draw_board(board, x_offset, hide_ships=False):
+    for y in range(GRID_SIZE):
+        for x in range(GRID_SIZE):
+            rect = pygame.Rect(x_offset + x * CELL_SIZE, MARGIN + y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, WHITE, rect, 1)
+            if board.grid[y][x] == -1:
+                pygame.draw.circle(screen, BLUE, rect.center, CELL_SIZE // 4)
+            elif board.grid[y][x] == -2:
+                pygame.draw.circle(screen, RED, rect.center, CELL_SIZE // 4)
+            elif isinstance(board.grid[y][x], Ship) and not hide_ships:
+                pygame.draw.rect(screen, GRAY, rect)
 
-    def cb(self):
-        print("Start button cb")
-        globals().update(game_state=State.PICK_SHIPS)
-        del self
-    startBtn = ClickableButton("Start", (250, 100), (middle.x - 125, middle.y + 200), cb)
+def player_place_ships(board, ships):
+    font = pygame.font.Font(None, 36)
+    
+    for ship in ships:
+        placing = True
+        horizontal = True
+        
+        while placing:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return False
+                
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    grid_x = x // CELL_SIZE
+                    grid_y = (y - MARGIN) // CELL_SIZE
+                    
+                    if 0 <= grid_x < GRID_SIZE and 0 <= grid_y < GRID_SIZE:
+                        if board.place_ship(ship, grid_x, grid_y, horizontal):
+                            placing = False
+                        else:
+                            print(f"Invalid placement at ({grid_x}, {grid_y}), horizontal: {horizontal}")
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        horizontal = not horizontal
+            
+            screen.fill(BLACK)
+            draw_board(board, 0)
+            
+            # Draw ship preview
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            grid_x = mouse_x // CELL_SIZE
+            grid_y = (mouse_y - MARGIN) // CELL_SIZE
+            
+            if 0 <= grid_x < GRID_SIZE and 0 <= grid_y < GRID_SIZE:
+                preview_ship = Ship(ship.size)
+                can_place = board.place_ship(preview_ship, grid_x, grid_y, horizontal)
+                preview_color = GREEN if can_place else RED
+                if horizontal:
+                    pygame.draw.rect(screen, preview_color, (grid_x * CELL_SIZE, MARGIN + grid_y * CELL_SIZE, ship.size * CELL_SIZE, CELL_SIZE), 2)
+                else:
+                    pygame.draw.rect(screen, preview_color, (grid_x * CELL_SIZE, MARGIN + grid_y * CELL_SIZE, CELL_SIZE, ship.size * CELL_SIZE), 2)
+                
+                # Remove the preview ship from the board
+                if can_place:
+                    board.ships.pop()
+                    for pos in preview_ship.positions:
+                        board.grid[pos[1]][pos[0]] = 0
+            
+            # Draw instructions
+            text = font.render(f"Place your ship of size {ship.size}. Press SPACE to rotate.", True, WHITE)
+            screen.blit(text, (10, 10))
+            
+            pygame.display.flip()
+    
+    return True
 
-    def txtCb(txt):
-        text = ''.join(txt)
-        match = re.match("[1-5]", text)
-        print(f"num ships: {match[0]}")
-        if match:
-            globals().update(game_state=State.PLAYER1START)
-            globals().update(num_ships=match[0])
-
-    shipTxtbox = TextBox(background, middle.x - 30, middle.y + 200, 60, 80, fontSize=50, onSubmit=txtCb)
-    shipTxtbox.onSubmitParams = [shipTxtbox.text]
-    boardGrid = Board()
-
-
-    ships = []
-    #these lines draw boxes for testing mouse movement --> either replace w/ ship object or delete
-    for i in range(5):
-        ship = Ship(i + 1)
-        #ship = pg.Rect(50 + i*40, 40, 30, 20)
-        ships.append(ship)
-    active_ship = None
-
+def ship_selection_screen():
+    font = pygame.font.Font(None, 36)
+    num_ships = 3  # Default number of ships
+    
+    running = True
     while running:
-        # poll for events
-        # pg.QUIT event means the user clicked X to close the window
-        events = pg.event.get()
-        for event in events:
-            if event.type == pg.QUIT:
-                running = False
-
-            #mouse movement
-            '''ROTATION IS STILL WONKY, NEEDS TO BE TWEAKED'''
-            if event.type == pg.MOUSEBUTTONDOWN: #if mouse clicked
-                for num, ship in enumerate(ships): #track index # of ships
-                    if ship.collidepoint(event.pos): #checks for collision w/ mouse coords
-                        active_ship = num #if collide, update active_ship w/ ship's index val
-                        '''if keys[pg.K_r]: #if r key pressed while dragging
-                            ships[active_ship].rotate90() #rotate ship'''
-                        break
-
-            if event.type == pg.MOUSEMOTION and active_ship != None: #if mouse moved and there is active ship
-                ships[active_ship].move(event.rel) #pick ship from list and move it by same amount as mouse
-                if keys[pg.K_r]: #if r key pressed while dragging
-                            print("r pressed") #debugging
-                            ships[active_ship].rotate90() #rotate ship
-
-            if event.type == pg.MOUSEBUTTONUP: #if mouse released
-                if active_ship is not None: #if ship active
-                    active_ship = None #releases ship
-                    break
-            
-
-        # fill the screen with a color to wipe away anything from last frame
-        background.fill("grey")
-        screen.fill("grey")
-
-        keys = pg.key.get_pressed()
-        if keys[pg.K_w]:
-            pass
-        if keys[pg.K_s]:
-            pass
-        if keys[pg.K_a]:
-            pass
-        if keys[pg.K_d]:
-            pass
-
-        if game_state == State.START:
-            # Put Img On The Background, Centered
-            # img, rect = load_image("battleship_fontbolt.png")
-            img = pg.image.load(os.path.join("data/battleship_fontbolt.png"))
-            img.convert()
-            img_size = img.get_size()
-            background.blit(img, (middle.x - img_size[0]/2, 100))
-
-            startBtn.draw(background, events)
-            startBtn.btn.show()
-            startBtn.btn.enable()
-        else:
-            # make start button is disabled
-            startBtn.btn.hide()
-            startBtn.btn.disable()
-
-        if game_state == State.PICK_SHIPS:
-            img = pg.image.load(os.path.join("data/battleship_fontbolt.png"))
-            img.convert()
-            img_size = img.get_size()
-            background.blit(img, (middle.x - img_size[0]/2, 100))
-
-            my_font = pg.font.Font(pg.font.get_default_font(), 36)
-            text_surface = my_font.render('Choose number of ships [1-5]', False, (0, 0, 0))
-            background.blit(text_surface, (middle.x - text_surface.get_width()/2, middle.y + 150))
-
-            shipTxtbox.draw()
-            shipTxtbox.show()
-            shipTxtbox.enable()
-        else:
-            shipTxtbox.hide()
-            shipTxtbox.disable()
-
-        if game_state == State.PLAYER1START:
-            size = 500
-            boardGrid.draw(background, middle.x - size/2, middle.y - size/2, size)
-
-            my_font = pg.font.Font(pg.font.get_default_font(), 56)
-            text_surface = my_font.render('PLAYER 1, PLACE SHIPS', False, (0, 0, 0))
-            background.blit(text_surface, (middle.x - text_surface.get_width()/2, 30))
-
-            #draw boxes ----> will have to replace w/ ship object       
-            for ship in ships:
-                #pg.draw.rect(background, "purple", ship)
-                if ships.index(ship) == active_ship:
-                    pg.draw.rect(background, "red", ship.rect.inflate(10, 10), 2)  # Draw a red outline on the ship player is actively moving
-                ship.draw(background)
-            
-            """
-            Outline for the rest of functionality
-            Place Ship object
-            Confirm placement
-            Change screen to a blank screen with a button that says
-            'Player two press to start turn'
-            Swithces to Player 2 start state when clicked
-            """
-        if game_state == State.PLAYER2START:
-            pass
-            '''
-            Copy of Player 1 start
-            Except confirmation sends the start to player 1 start
-            '''
+        screen.fill(BLACK)
         
-        if game_state == State.PLAYER1TURN:
-            pass
-            '''
-            Look at hits on their board
-            Moves to attack phase and confirms hits
-            Check victory condition
-            on miss switch to transition screen
-            switch to player 2 turn
-            '''
-
-        if game_state == State.PLAYER2TURN:
-            pass
-            '''
-            Look at hits on their board
-            Moves to attack phase and confirms hits
-            Check victory condition
-            on miss switch to transition screen
-            switch to player 1 turn
-            '''
-            
-
-        screen.blit(background, (0, 0))
-        pw.update(events)  # Call once every loop to allow widgets to render and listen
+        # Display current number of ships
+        text = font.render(f"Number of ships: {num_ships}", True, WHITE)
+        screen.blit(text, (300, 200))
         
-        # flip() the display to put the work we did on screen
-        #pg.display.update()
-        pg.display.flip()
+        # Plus button
+        pygame.draw.rect(screen, WHITE, (500, 200, 30, 30), 2)
+        plus = font.render("+", True, WHITE)
+        screen.blit(plus, (510, 205))
+        
+        # Minus button
+        pygame.draw.rect(screen, WHITE, (550, 200, 30, 30), 2)
+        minus = font.render("-", True, WHITE)
+        screen.blit(minus, (560, 205))
+        
+        # Start game button
+        pygame.draw.rect(screen, GREEN, (300, 300, 200, 50))
+        start_text = font.render("Start Game", True, BLACK)
+        screen.blit(start_text, (340, 315))
+        
+        # Display ship sizes
+        ship_sizes_text = font.render(f"Ship sizes: {', '.join(str(i) for i in range(1, num_ships + 1))}", True, WHITE)
+        screen.blit(ship_sizes_text, (300, 250))
+        
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return None
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                if 300 < x < 500 and 300 < y < 350:
+                    return [Ship(i) for i in range(1, num_ships + 1)]
+                elif 500 < x < 530 and 200 < y < 230:
+                    num_ships = min(num_ships + 1, 5)
+                elif 550 < x < 580 and 200 < y < 230:
+                    num_ships = max(num_ships - 1, 1)
 
-        tick = clock.tick(60) # limits FPS to 60
+def transition_screen(player_number):
+    font = pygame.font.Font(None, 48)
+    screen.fill(BLACK)
+    text = font.render(f"Player {player_number}'s Turn", True, WHITE)
+    text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
+    time.sleep(2)  # Display the transition screen for 2 seconds
 
-    pg.quit()
+import pygame
+import random
+import time
+
+# ... [Previous code remains unchanged up to the main() function]
+
+def display_attack_result(screen, result, x, y):
+    font = pygame.font.Font(None, 36)
+    if result:
+        text = font.render("Hit!", True, RED)
+    else:
+        text = font.render("Miss!", True, BLUE)
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
+    time.sleep(1.5)  # Display the result for 1.5 seconds
 
 def main():
-    # run main loop
-    run()
+    ships = ship_selection_screen()
+    if not ships:
+        return
+
+    player1_board = Board()
+    player2_board = Board()
+
+    # Player 1 ship placement
+    transition_screen(1)
+    if not player_place_ships(player1_board, ships):
+        return
+
+    # Player 2 ship placement
+    transition_screen(2)
+    if not player_place_ships(player2_board, ships):
+        return
+
+    current_player = 1
+    game_over = False
+
+    while not game_over:
+        transition_screen(current_player)
+        attacking_board = player2_board if current_player == 1 else player1_board
+        
+        screen.fill(BLACK)
+        draw_board(attacking_board, 0, hide_ships=True)
+        pygame.display.flip()
+
+        waiting_for_move = True
+        while waiting_for_move:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    grid_x = x // CELL_SIZE
+                    grid_y = (y - MARGIN) // CELL_SIZE
+                    if 0 <= grid_x < GRID_SIZE and 0 <= grid_y < GRID_SIZE:
+                        hit = attacking_board.receive_attack(grid_x, grid_y)
+                        screen.fill(BLACK)
+                        draw_board(attacking_board, 0, hide_ships=True)
+                        display_attack_result(screen, hit, SCREEN_WIDTH // 2, SCREEN_HEIGHT - 50)
+                        
+                        if hit and all(ship.is_sunk() for ship in attacking_board.ships):
+                            font = pygame.font.Font(None, 48)
+                            win_text = font.render(f"Player {current_player} wins!", True, GREEN)
+                            win_rect = win_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+                            screen.blit(win_text, win_rect)
+                            pygame.display.flip()
+                            time.sleep(3)  # Display win message for 3 seconds
+                            game_over = True
+                        waiting_for_move = False
+
+        if not game_over:
+            current_player = 3 - current_player  # Switch players (1 -> 2, 2 -> 1)
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
